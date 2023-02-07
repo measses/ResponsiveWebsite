256b9f242706cb751fc3a34fb3c8b5168d072b46
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2023-02-08 01:21:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var menuItems=document.getElementById('menuItems');
+menuItems.style.maxHeight="0px";
+
+function menuToggle(){
+    if(menuItems.style.maxHeight=="0px"){
+        menuItems.style.maxHeight="200px";
+
+    }else{
+        menuItems.style.maxHeight="0px"
+    }
+}
+
